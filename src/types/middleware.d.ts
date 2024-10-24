3ade349b26d0d855f4aec85df0b5c8b3c5b7a7e3
Icarus <63220735+shoguncoffee@users.noreply.github.com>
2024-10-25 03:28:31 +0700

    adjust noImplicitAny setting

--- src/types/middleware.d.ts
+++ src/types/middleware.d.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
+import { Types } from 'mongoose'
 import { JwtPayload } from 'jsonwebtoken';
 import { Request as JwtRequest } from 'express-jwt';
 
@@ -6,7 +7,7 @@ import { Request as JwtRequest } from 'express-jwt';
 declare global {
     type middleware = (req: Request, res: Response, next: NextFunction) => void;
 
-    type AuthRequest<U> = JwtRequest<{ account: U }>;
+    type AuthRequest<U> = JwtRequest<{ account: U, account_id: Types.ObjectId}>;
     type StanderRequest = AuthRequest<TStander>;
     type CustomerRequest = AuthRequest<TCustomer>;
     type AccountRequest = AuthRequest<TAccount>;
